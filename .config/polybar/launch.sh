3e37b2a951c36060336bf2934fe0f3db09632e0b
sanamorii <AHHHHHH>
2023-03-02 18:02:50 +0000

    initial

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+polybar-msg cmd quit
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "polybar executed"
